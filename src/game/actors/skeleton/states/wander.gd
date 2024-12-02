b8c256e8c02260901819d0d3f163bab816f703e2
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-02 00:10:14 -0500

    Skeleton states and directional animations
    Also set physics

---
+++ src/game/actors/skeleton/states/wander.gd
@@ -0,0 +1,17 @@
+extends State
+
+
+func enter() -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func process(_delta: float) -> StringName:
+	return &""
+
+
+func physics_process(_delta: float) -> StringName:
+	return &""
