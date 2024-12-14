7bd5ffc59674bb754d1414b30b951038549bd22a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-14 12:49:47 -0500

    Fix how directional animation handles diagonals
    Also set knockback animations to not change with direction

--- src/core/actors/direction_animation_player.gd
+++ src/core/actors/direction_animation_player.gd
@@ -18,10 +18,9 @@ var _last_cardinal := Vector2.ZERO
 func _ready() -> void:
 	for anim_set in animation_sets:
 		_anim_sets_by_name[anim_set.anim_set_name] = anim_set
-	set_animation_set(animation_sets[0].anim_set_name)
 
 	animation_player.animation_finished.connect(_animation_finished)
-	_update_animation()
+	set_animation_set(animation_sets[0].anim_set_name)
 
 
 func _process(_delta: float) -> void:
@@ -35,29 +34,27 @@ func set_animation_set(anim_set_name: StringName) -> void:
 
 
 func _update_animation() -> void:
-	_current_anim_name = _get_current_anim_set().get_animation_name(
-			motion.direction)
 	_last_cardinal = _get_current_cardinal()
-	animation_player.play(_current_anim_name)
+	var new_anim_name := _get_current_anim_set().get_animation_name(
+			_last_cardinal)
+	if new_anim_name != _current_anim_name:
+		_current_anim_name = new_anim_name
+		animation_player.play(_current_anim_name)
 
 
 func _get_current_cardinal() -> Vector2:
-	var result: Vector2
-	# If moving perfectly diagonal, face in last cardinal direction if it's an
-	# equivalent facing.
-	if \
-			( \
-				absf(motion.direction.x) == absf(motion.direction.y)
-			) \
-			and \
-			( \
-				(signf(_last_cardinal.x) == signf(motion.direction.x)) \
-				or \
-				(signf(_last_cardinal.y) == signf(motion.direction.y)) \
-			):
+	var result := _get_current_anim_set().get_closest_cardinal(motion.direction)
+
+	var is_moving := motion.velocity.length_squared() > 0
+	var is_diagonal := is_equal_approx(
+				absf(motion.direction.x), absf(motion.direction.y))
+	var is_aligned_with_cardinal := \
+			(signf(_last_cardinal.x) == signf(motion.direction.x)) \
+			or \
+			(signf(_last_cardinal.y) == signf(motion.direction.y))
+
+	if is_moving and is_diagonal and is_aligned_with_cardinal:
 		result = _last_cardinal
-	else:
-		result = _get_current_anim_set().get_closest_cardinal(motion.direction)
 
 	return result
 
