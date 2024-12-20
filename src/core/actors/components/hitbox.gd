27defcbc13a81020b20f924ad8fe299310f7080e
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-19 20:23:52 -0500

    PhysicsConstants

--- src/core/actors/components/hitbox.gd
+++ src/core/actors/components/hitbox.gd
@@ -4,8 +4,6 @@ extends Area2D
 
 ## A shape that does damage to [Hurtbox]es.
 
-const HITBOX_LAYER := 3
-
 @export var faction := 0
 @export var damage := 1
 
@@ -20,7 +18,7 @@ const HITBOX_LAYER := 3
 func _ready() -> void:
 	collision_layer = 0
 	collision_mask = 0
-	set_collision_layer_value(HITBOX_LAYER, true)
+	set_collision_layer_value(PhysicsConstants.HITBOX_LAYER, true)
 
 	monitoring = false
 	active = active
---
+++ src/core/actors/components/physics_constants.gd
@@ -0,0 +1,4 @@
+class_name PhysicsConstants
+
+const HITBOX_LAYER := 3
+const HURTBOX_LAYER := 4
