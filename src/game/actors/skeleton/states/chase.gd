06b16646369019699d036f78f5c52177af4745c8
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-20 22:32:13 -0500

    Skeleton melee attacks

--- src/game/actors/skeleton/states/chase.gd
+++ src/game/actors/skeleton/states/chase.gd
@@ -1,12 +1,15 @@
 extends ActorState
 
 @export var actor_motion: ActorMotion
-
 @export var acceleration := 10.0
 @export var max_speed := 100.0
 
 @export var sight_radius := 150
 @export var idle_state := &"Idle"
+
+@export var attack_pivot: Node2D
+@export var attack_radius := 24
+@export var attack_state := &"Attack"
 
 var _body: Node2D
 
@@ -18,6 +21,8 @@ func _ready() -> void:
 func _process_main(_delta: float) -> StringName:
 	var target_vector := _vector_to_target()
 
+	if target_vector.length_squared() <= (attack_radius * attack_radius):
+		return attack_state
 	if target_vector.length_squared() > (sight_radius * sight_radius):
 		return idle_state
 
@@ -27,6 +32,6 @@ func _process_main(_delta: float) -> StringName:
 
 
 func _vector_to_target() -> Vector2:
-	var player_pos := Globals.player.position
-	var self_pos := _body.position
+	var player_pos := Globals.player.position + attack_pivot.position
+	var self_pos := _body.position + attack_pivot.position
 	return player_pos - self_pos
--- src/game/actors/skeleton/states/idle.gd
+++ src/game/actors/skeleton/states/idle.gd
@@ -1,11 +1,14 @@
 extends ActorState
 
+@export var actor_motion: ActorMotion
 @export var deceleration := 12.0
 
 @export var sight_radius := 150
 @export var chase_state := &""
 
-@export var actor_motion: ActorMotion
+@export var attack_pivot: Node2D
+@export var attack_radius := 24
+@export var attack_state := &"Attack"
 
 var _body: Node2D
 
@@ -17,10 +20,17 @@ func _ready() -> void:
 func _process_main(_delta: float) -> StringName:
 	actor_motion.move_velocity_toward(Vector2.ZERO, deceleration)
 
-	var player_pos := Globals.player.position
-	var self_pos := _body.position
-	var distance_sqrd := (player_pos - self_pos).length_squared()
-	if distance_sqrd <= (sight_radius * sight_radius):
+	var target_vector := _vector_to_target()
+
+	if target_vector.length_squared() <= (attack_radius * attack_radius):
+		return attack_state
+	elif target_vector.length_squared() <= (sight_radius * sight_radius):
 		return chase_state
 
 	return &""
+
+
+func _vector_to_target() -> Vector2:
+	var player_pos := Globals.player.position + attack_pivot.position
+	var self_pos := _body.position + attack_pivot.position
+	return player_pos - self_pos
