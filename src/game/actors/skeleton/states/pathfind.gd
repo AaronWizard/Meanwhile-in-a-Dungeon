4897ebdada3b7181f33d27de144b115a68df6b20
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-22 14:43:56 -0500

    Revert "Surround state"
    This reverts commit 4d5a3436dd20aa9eca86e8fe12f39997e73d27b2.

--- src/game/actors/skeleton/states/pathfind.gd
+++ src/game/actors/skeleton/states/pathfind.gd
@@ -9,12 +9,13 @@ extends ActorState
 @export var nav_agent: NavigationAgent2D
 @export var idle_state := &"Idle"
 
-@export_group("Surround")
-@export var surround_range := 64
-@export var surround_state := &"Surround"
+@export_group("Attack")
+@export var attack_range: Area2D
+@export var attack_state := &"Attack"
 
 var _body: Node2D
 var _active := false
+var _can_attack := false
 
 @onready var _target_update_timer := $TargetUpdateTimer as Timer
 
@@ -22,7 +23,10 @@ var _active := false
 func _ready() -> void:
 	_body = owner as Node2D
 
+	attack_range.monitoring = false
+
 	nav_agent.velocity_computed.connect(_on_velocity_computed)
+	attack_range.body_entered.connect(_on_actor_entered)
 	_target_update_timer.timeout.connect(_on_target_update_timer_timeout)
 
 
@@ -30,21 +34,28 @@ func enter() -> void:
 	super()
 
 	_active = true
+	_can_attack = false
 
 	_on_target_update_timer_timeout() # Set chase target
 	_target_update_timer.start()
 
+	attack_range.monitoring = true
+
 
 func exit() -> void:
 	_active = false
+	_can_attack = false
+
 	_target_update_timer.stop()
+	attack_range.monitoring = false
 
 
 func _process_main(_delta: float) -> StringName:
 	if not _active:
 		return idle_state
-	if _vector_to_target().length_squared() <= (surround_range * surround_range):
-		return surround_state
+
+	if _can_attack:
+		return attack_state
 
 	return &""
 
@@ -73,6 +84,11 @@ func _vector_to_target() -> Vector2:
 func _on_velocity_computed(velocity: Vector2) -> void:
 	if _active:
 		actor_motion.velocity = velocity
+
+
+func _on_actor_entered(actor: Actor) -> void:
+	if actor and (actor == Globals.player):
+		_can_attack = true
 
 
 func _on_target_update_timer_timeout() -> void:
