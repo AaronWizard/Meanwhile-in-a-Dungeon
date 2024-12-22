7dccbe6de595afcf47d3d7057b0a8e9b31ad30d0
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-21 23:36:15 -0500

    Refinement of skeleton detection and attack range

--- src/game/actors/skeleton/states/idle.gd
+++ src/game/actors/skeleton/states/idle.gd
@@ -1,35 +1,39 @@
 extends ActorState
 
+@export_group("Motion")
 @export var actor_motion: ActorMotion
 @export var deceleration := 12.0
 
-@export var sight_radius := 150
-@export var chase_state := &""
-
-@export var attack_radius := 24
-@export var attack_state := &"Attack"
+@export_group("Pursuit")
+@export var enemy_detector: Area2D
+@export var pathfind_state := &"Pathfind"
 
 var _body: Node2D
+var _enemy_found := false
 
 
 func _ready() -> void:
 	_body = owner as Node2D
+	enemy_detector.monitoring = false
+	enemy_detector.body_entered.connect(_on_actor_entered)
+
+
+func enter() -> void:
+	enemy_detector.monitoring = true
+
+
+func exit() -> void:
+	enemy_detector.monitoring = false
+	_enemy_found = false
 
 
 func _process_main(_delta: float) -> StringName:
 	actor_motion.move_velocity_toward(Vector2.ZERO, deceleration)
-
-	var target_vector := _vector_to_target()
-
-	if target_vector.length_squared() <= (attack_radius * attack_radius):
-		return attack_state
-	elif target_vector.length_squared() <= (sight_radius * sight_radius):
-		return chase_state
-
+	if _enemy_found:
+		return pathfind_state
 	return &""
 
 
-func _vector_to_target() -> Vector2:
-	var player_pos := Globals.player.position
-	var self_pos := _body.position
-	return player_pos - self_pos
+func _on_actor_entered(actor: Actor) -> void:
+	if actor and (actor == Globals.player):
+		_enemy_found = true
--- src/game/actors/skeleton/states/pathfind.gd
+++ src/game/actors/skeleton/states/pathfind.gd
@@ -1,51 +1,60 @@
 extends ActorState
 
+@export_group("Motion")
 @export var actor_motion: ActorMotion
 @export var acceleration := 10.0
 @export var max_speed := 100.0
 
+@export_group("Pursuit")
 @export var nav_agent: NavigationAgent2D
-
-@export var sight_radius := 150
 @export var idle_state := &"Idle"
 
-@export var attack_radius := 24
+@export_group("Attack")
+@export var attack_range: Area2D
 @export var attack_state := &"Attack"
 
 var _body: Node2D
-var _keep_chasing := false
+var _active := false
+var _can_attack := false
 
 @onready var _target_update_timer := $TargetUpdateTimer as Timer
-@onready var _chase_timer: Timer = $ChaseTimer
 
 
 func _ready() -> void:
 	_body = owner as Node2D
+
+	attack_range.monitoring = false
+
 	nav_agent.velocity_computed.connect(_on_velocity_computed)
+	attack_range.body_entered.connect(_on_actor_entered)
+	_target_update_timer.timeout.connect(_on_target_update_timer_timeout)
 
 
 func enter() -> void:
 	super()
 
-	_keep_chasing = true
-	_set_chase_target()
+	_active = true
+	_can_attack = false
 
+	_on_target_update_timer_timeout() # Set chase target
 	_target_update_timer.start()
-	_chase_timer.start()
+
+	attack_range.monitoring = true
 
 
 func exit() -> void:
-	_keep_chasing = false
+	_active = false
+	_can_attack = false
+
 	_target_update_timer.stop()
-	_chase_timer.stop()
+	attack_range.monitoring = false
 
 
 func _process_main(_delta: float) -> StringName:
-	if not _keep_chasing:
+	if not _active:
 		return idle_state
 
-	var target_vector := _vector_to_target()
-	if target_vector.length_squared() <= (attack_radius * attack_radius):
+	if _can_attack:
 		return attack_state
 
 	return &""
@@ -54,9 +63,9 @@ func _process_main(_delta: float) -> StringName:
 func physics_process(_delta: float) -> StringName:
 	if NavigationServer2D.map_get_iteration_id(nav_agent.get_navigation_map()) \
 			== 0:
-		return &""
+		return idle_state
 	if nav_agent.is_navigation_finished():
-		return &""
+		return idle_state
 
 	var next_pos := nav_agent.get_next_path_position()
 	var next_velocity := _body.global_position.direction_to(next_pos) \
@@ -72,16 +81,15 @@ func _vector_to_target() -> Vector2:
 	return player_pos - self_pos
 
 
-func _on_velocity_computed(safe_velocity: Vector2) -> void:
-	if _keep_chasing:
-		actor_motion.velocity = safe_velocity
+func _on_velocity_computed(velocity: Vector2) -> void:
+	if _active:
+		actor_motion.velocity = velocity
 
 
-func _set_chase_target() -> void:
+func _on_actor_entered(actor: Actor) -> void:
+	if actor and (actor == Globals.player):
+		_can_attack = true
+
+
+func _on_target_update_timer_timeout() -> void:
 	nav_agent.target_position = Globals.player.position
-
-
-func _on_chase_timer_timeout() -> void:
-	var target_vector := _vector_to_target()
-	if target_vector.length_squared() > (sight_radius * sight_radius):
-		_keep_chasing = false
