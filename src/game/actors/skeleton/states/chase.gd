a3d31c22449f2da5001d142dd71e91ba20469df8
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-27 00:59:22 -0500

    Fix switching between move and idle animation

--- src/game/actors/skeleton/states/chase.gd
+++ src/game/actors/skeleton/states/chase.gd
@@ -17,7 +17,7 @@ func exit() -> void:
 
 
 func process(_delta: float) -> StringName:
-	if is_equal_approx(motion.velocity.length_squared(), 0.0):
+	if motion.velocity.length_squared() <= 2.0:
 		direction_animation_player.set_animation_set(anim_set_idle)
 	else:
 		direction_animation_player.set_animation_set(anim_set_motion)
