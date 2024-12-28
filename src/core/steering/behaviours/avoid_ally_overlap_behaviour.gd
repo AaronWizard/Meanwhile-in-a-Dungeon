19ad9aed0d93fd94b10993828c32f552f9dd8dee
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2024-12-28 17:04:18 -0500

    Improve AvoidAllyOverlapBehaviour

--- src/core/steering/behaviours/avoid_ally_overlap_behaviour.gd
+++ src/core/steering/behaviours/avoid_ally_overlap_behaviour.gd
@@ -2,7 +2,8 @@ class_name AvoidAllyOverlapBehaviour
 extends SteeringBehaviour
 
 @export var body: Node2D
-@export var avoid_radius := 32.0
+@export var avoid_radius := 16.0
+@export var mult := 4.0
 @export var actor_detector: ActorDetector
 
 
@@ -10,14 +11,12 @@ func _fill_context_map(context_map: SteeringContextMap, _delta: float) -> void:
 	for a in actor_detector.visible_actors:
 		if _is_ally(a):
 			var vector := _actor_vector(a)
-			var direction := vector.normalized()
 			var distance := vector.length()
+			var direction := vector / distance
 
 			var diff := clampf((avoid_radius - distance) / avoid_radius,
 					0.0, 1.0)
-			if distance < avoid_radius:
-				context_map.assign_interest_vector(-direction * diff)
-			context_map.assign_danger_vector(direction * diff)
+			context_map.assign_danger_vector(direction * diff * mult)
 
 
 func _is_ally(actor: Actor) -> bool:
