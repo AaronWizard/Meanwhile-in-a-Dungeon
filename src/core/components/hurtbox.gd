b8d21ff8fdae1a441de6f72f5f59bbd3e3df64d0
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2025-01-07 00:23:17 -0500

    Restored hit sounds

--- src/core/components/hurtbox.gd
+++ src/core/components/hurtbox.gd
@@ -5,6 +5,8 @@ extends Area2D
 
 signal was_hit(damage: int, direction: Vector2)
 
+@export var hit_sound_2d: AudioStreamPlayer2D
+
 
 func _ready() -> void:
 	area_entered.connect(_hitbox_entered)
@@ -14,3 +16,5 @@ func _hitbox_entered(hitbox: Hitbox) -> void:
 	if hitbox:
 		var direction := (global_position - hitbox.global_position).normalized()
 		was_hit.emit(hitbox.damage, direction)
+		if hit_sound_2d:
+			hit_sound_2d.play()
